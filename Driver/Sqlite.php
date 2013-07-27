6a065f363a9c989d3a88c7eadb73a0b7f81d03ec
Sascha Weyers <kontakt@sw-is.de>
2013-07-27 13:26:57 +0200

    first commit

---
+++ Driver/Sqlite.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Swis\Bundle\DatabaseExtraBundle\Driver;
+
+class Sqlite extends \Doctrine\DBAL\Driver\PDOSqlite\Driver
+{
+
+    protected $_userDefinedFunctions = array(
+        'sqrt' => array('callback' => array('Doctrine\DBAL\Platforms\SqlitePlatform', 'udfSqrt'), 'numArgs' => 1),
+        'mod'  => array('callback' => array('Doctrine\DBAL\Platforms\SqlitePlatform', 'udfMod'), 'numArgs' => 2),
+        'locate'  => array('callback' => array('Doctrine\DBAL\Platforms\SqlitePlatform', 'udfLocate'), 'numArgs' => -1),
+        'acos'  => array('callback' => 'acos', 'numArgs' => 1),
+        'cos'  => array('callback' => 'cos', 'numArgs' => 1),
+        'radians'  => array('callback' => array('Talents\Bundle\DatabaseExtraBundle\Driver\Sqlite', 'radians'), 'numArgs' => -1),
+        'sin'  => array('callback' => 'sin', 'numArgs' => 1),
+    );
+
+    public static function radians($arg)
+    {
+        return \deg2rad($arg);
+    }
+}
---
+++ SwisDatabaseExtraBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Swis\Bundle\DatabaseExtraBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class DnDatabaseExtraBundle extends Bundle
+{
+}
