8b8250ab0a00cf207ca262df3532b707fe568397
Sascha Weyers <kontakt@sw-is.de>
2013-08-17 11:27:39 +0200

    fix namespace

--- Driver/Sqlite.php
+++ Driver/Sqlite.php
@@ -11,7 +11,7 @@ class Sqlite extends \Doctrine\DBAL\Driver\PDOSqlite\Driver
         'locate'  => array('callback' => array('Doctrine\DBAL\Platforms\SqlitePlatform', 'udfLocate'), 'numArgs' => -1),
         'acos'  => array('callback' => 'acos', 'numArgs' => 1),
         'cos'  => array('callback' => 'cos', 'numArgs' => 1),
-        'radians'  => array('callback' => array('Talents\Bundle\DatabaseExtraBundle\Driver\Sqlite', 'radians'), 'numArgs' => -1),
+        'radians'  => array('callback' => array('Swis\Bundle\DatabaseExtraBundle\Driver\Sqlite', 'radians'), 'numArgs' => -1),
         'sin'  => array('callback' => 'sin', 'numArgs' => 1),
     );
 
